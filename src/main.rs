7300733e4baced3498779d720c8a7e8919e507dd
Alexander Shabalin <shabalyn.a@gmail.com>
2019-05-18 14:08:04 +0300

    Start on Lua syntax

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 #[macro_use]
 mod parser_lib;
+mod lua_syntax;
 
 fn main() {
     println!("Hello, world!");
+    println!("{}", lua_syntax::Keyword::items_count());
+    for keyword in lua_syntax::Keyword::ITEMS.into_iter() {
+        println!("{:?} => {}", keyword, keyword);
+    }
+    println!(
+        "{:?} {:?}",
+        lua_syntax::Keyword::create_sorted_items(),
+        lua_syntax::Keyword::ITEMS
+    );
 }
