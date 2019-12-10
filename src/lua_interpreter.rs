12d6ace79adab9aaa7efefba63c4cbbe04725783
Alexander Shabalin <shabalyn.a@gmail.com>
2019-12-10 15:30:02 +0300

    Start on interpreter

--- src/lua_interpreter.rs
+++ src/lua_interpreter.rs
@@ -1,6 +1,35 @@
 use crate::lua_syntax;
 
 pub fn interpret(ast: lua_syntax::Block) -> Result<(), String> {
-    println!("TODO: {:?}", ast);
+    let mut s = Scope{};
+    interpret_block(&s, ast)
+}
+
+struct Scope {}
+
+impl Scope {
+    fn inherit(s: &Scope) -> Scope {
+        Scope{}
+    }
+}
+
+fn interpret_block(s: &Scope, ast: lua_syntax::Block) -> Result<(), String> {
+    let mut s = Scope::inherit(s);
+    for stmt in ast.0 {
+        interpret_statement(&s, stmt)?;
+    }
+    if let Some(stmt) = ast.1 {
+        interpret_last_statement(&s, stmt)?;
+    }
+    Ok(())
+}
+
+fn interpret_statement(s: &Scope, stmt: lua_syntax::Stat) -> Result<(), String> {
+    eprintln!("stmt {:?}", stmt);
+    Ok(())
+}
+
+fn interpret_last_statement(s: &Scope, stmt: lua_syntax::LastStat) -> Result<(), String> {
+    eprintln!("stmt {:?}", stmt);
     Ok(())
 }
