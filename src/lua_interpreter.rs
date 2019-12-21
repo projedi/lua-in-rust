6c24ad03c9b6f16b4a5ca3760a41445c72b298d3
Alexander Shabalin <shabalyn.a@gmail.com>
2019-12-21 11:36:47 +0300

    Remove Value from interperter

--- src/lua_interpreter.rs
+++ src/lua_interpreter.rs
@@ -1,5 +1,5 @@
-use crate::lua_lexemes;
 use crate::lua_syntax;
+use crate::lua_interpreter::value::{TableRef, Value, Number, adjust_values, adjust_values_1};
 
 mod value;
 
@@ -29,135 +29,18 @@ impl Scope {
     fn get_table(&self, name: lua_syntax::Name) -> TableRef {
         // TODO: Implement
         eprintln!("get_table {:?}", name);
-        TableRef()
+        TableRef::make(&[])
     }
 }
 
-#[derive(Debug, Clone)]
-struct TableRef();
-
-impl TableRef {
-    fn get_field<'a, 'b>(&'a self, field: &'b str) -> Value<'a> {
-        // TODO: Implement
-        eprintln!("get_field {:?} {:?}", self, field);
-        Value::Nil
-    }
-
-    fn set_field<'a, 'b>(&'a self, field: &'b str, val: Value<'a>) {
-        // TODO: Implement
-        eprintln!("set_field {:?} {:?} {:?}", self, field, val);
-    }
-
-    fn len(&self) -> f64 {
-        // TODO: Implement.
-        eprintln!("len {:?}", self);
-        0 as f64
-    }
-}
-
-#[derive(Debug, Clone)]
-struct StringRef();
-
-impl StringRef {
-    fn len(&self) -> f64 {
-        // TODO: Implement.
-        eprintln!("len {:?}", self);
-        0 as f64
-    }
-}
-
-type Number = f64;
-
-#[derive(Debug, Clone)]
-enum Value<'a> {
-    Nil,
-    Number(Number),
-    StaticString(lua_syntax::StringLiteral<'a>),
-    String(StringRef),
-    Table(TableRef),
-    Bool(bool),
-}
-
-impl<'a> Value<'a> {
-    fn is_nil(&self) -> bool {
-        match self {
-            Value::Nil => true,
-            _ => false,
-        }
-    }
-
-    fn to_bool(&self) -> bool {
-        match *self {
-            Value::Nil => false,
-            Value::Number(_) => true,
-            Value::StaticString(_) => true,
-            Value::String(_) => true,
-            Value::Table(_) => true,
-            Value::Bool(b) => b,
-        }
-    }
-
-    fn to_number(&self) -> Option<Number> {
-        match self {
-            Value::Nil => None,
-            Value::Number(n) => Some(*n),
-            // TODO: Implement conversion from string.
-            Value::StaticString(_) => None,
-            // TODO: Implement conversion from string.
-            Value::String(_) => None,
-            Value::Table(_) => None,
-            Value::Bool(_) => None,
-        }
-    }
-
-    fn get_field(&'a self, field: lua_syntax::Name) -> Value<'a> {
-        match self {
-            Value::Nil => Value::Nil,
-            // TODO: What happens in lua?
-            Value::Number(_) => Value::Nil,
-            // TODO: What happens in lua?
-            Value::StaticString(_) => Value::Nil,
-            // TODO: What happens in lua?
-            Value::String(_) => Value::Nil,
-            // TODO: What happens in lua?
-            Value::Bool(_) => Value::Nil,
-            Value::Table(t) => t.get_field(field),
-        }
-    }
-
-    fn set_field<'b>(&'a self, field: lua_syntax::Name, val: Value<'b>) {
-        match self {
-            Value::Table(t) => t.set_field(field, val),
-            // TODO: What happens in lua?
-            _ => {}
-        }
-    }
-
-    fn metadata(&self) -> TableRef {
-        // TODO: Implement.
-        eprintln!("metadata {:?}", self);
-        TableRef()
-    }
-
-    fn len(&self) -> Option<f64> {
-        match self {
-            Value::StaticString(lua_lexemes::StringLiteral::QuotedStringLiteral(s)) => Some(s.string.len() as f64),
-            Value::StaticString(lua_lexemes::StringLiteral::RawStringLiteral(s)) => Some(s.string.len() as f64),
-            Value::String(s) => Some(s.len()),
-            Value::Table(t) => Some(t.len()),
-            _ => None,
-        }
-    }
-}
-
-enum StatementError<'a> {
-    Return(Vec<Value<'a>>),
+enum StatementError<'s, 'g> {
+    Return(Vec<Value<'s, 'g>>),
     Break,
     Error(String)
 }
 
 // Scope creation is left to the callers.
-fn interpret_block<'a>(s: &Scope, block: &lua_syntax::Block<'a>) -> Result<(), StatementError<'a>> {
+fn interpret_block<'s, 'g>(s: &Scope, block: &lua_syntax::Block<'s>) -> Result<(), StatementError<'s, 'g>> {
     for stmt in &block.0 {
         interpret_statement(s, stmt)?;
     }
@@ -167,25 +50,13 @@ fn interpret_block<'a>(s: &Scope, block: &lua_syntax::Block<'a>) -> Result<(), S
     Ok(())
 }
 
-fn adjust_values(mut vals: Vec<Value>, count: usize) -> Vec<Value> {
-    vals.resize_with(count, || Value::Nil);
-    vals
-}
-
-fn adjust_single_value(vals: Vec<Value>) -> Value {
-    match vals.into_iter().next() {
-        Some(val) => val,
-        None => Value::Nil,
-    }
-}
-
-fn assign_var<'a>(s: &Scope, var: &lua_syntax::Var<'a>, val: Value<'a>) -> Result<(), StatementError<'a>> {
+fn assign_var<'s, 'g>(s: &Scope, var: &lua_syntax::Var<'s>, val: Value<'s, 'g>) -> Result<(), StatementError<'s, 'g>> {
     // TODO: Implement. Do not forget about metatables.
     eprintln!("assign_var {:?} {:?}", var, val);
     Ok(())
 }
 
-fn assign<'a>(s: &Scope, vars: &[lua_syntax::Var<'a>], exps: Vec<Value<'a>>) -> Result<(), StatementError<'a>> {
+fn assign<'s, 'g>(s: &Scope, vars: &[lua_syntax::Var<'s>], exps: Vec<Value<'s, 'g>>) -> Result<(), StatementError<'s ,'g>> {
     let exps = adjust_values(exps, vars.len());
     for (var, val) in vars.iter().zip(exps) {
         assign_var(s, var, val)?;
@@ -193,7 +64,7 @@ fn assign<'a>(s: &Scope, vars: &[lua_syntax::Var<'a>], exps: Vec<Value<'a>>) -> 
     Ok(())
 }
 
-fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(), StatementError<'a>> {
+fn interpret_statement<'s, 'g>(s: &Scope, stmt: &lua_syntax::Stat<'s>) -> Result<(), StatementError<'s, 'g>> {
     match stmt {
         lua_syntax::Stat::Assign(vars, exps) => {
             let vals = interpret_exps(s, exps)?;
@@ -209,7 +80,7 @@ fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(),
         }
         lua_syntax::Stat::While(exp, block) => {
             loop {
-                if !adjust_single_value(interpret_exp(s, exp)?).to_bool() {
+                if !adjust_values_1(interpret_exp(s, exp)?).to_bool() {
                     return Ok(())
                 }
                 let s = Scope::inherit(s);
@@ -220,19 +91,19 @@ fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(),
             loop {
                 let s = Scope::inherit(s);
                 interpret_block(&s, block)?;
-                if adjust_single_value(interpret_exp(&s, exp)?).to_bool() {
+                if adjust_values_1(interpret_exp(&s, exp)?).to_bool() {
                     return Ok(())
                 }
             }
         }
         lua_syntax::Stat::If(ifBlock, ifElseBlocks, elseBlock) => {
             let (e, block) = ifBlock;
-            if adjust_single_value(interpret_exp(&s, e)?).to_bool() {
+            if adjust_values_1(interpret_exp(&s, e)?).to_bool() {
                 let s = Scope::inherit(s);
                 return interpret_block(&s, block);
             }
             for (e, block) in ifElseBlocks {
-                if adjust_single_value(interpret_exp(&s, e)?).to_bool() {
+                if adjust_values_1(interpret_exp(&s, e)?).to_bool() {
                     let s = Scope::inherit(s);
                     return interpret_block(&s, block);
                 }
@@ -244,10 +115,10 @@ fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(),
             Ok(())
         }
         lua_syntax::Stat::For(var, init, limit, step, block) => {
-            let init = adjust_single_value(interpret_exp(&s, init)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?;
-            let limit = adjust_single_value(interpret_exp(&s, limit)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?;
+            let init = adjust_values_1(interpret_exp(&s, init)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?;
+            let limit = adjust_values_1(interpret_exp(&s, limit)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?;
             let step = match step {
-                Some(step) => adjust_single_value(interpret_exp(&s, step)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?,
+                Some(step) => adjust_values_1(interpret_exp(&s, step)?).to_number().ok_or(StatementError::Error("Expected a number".to_string()))?,
                 None => 1 as Number,
             };
             let s = Scope::inherit(s);
@@ -256,7 +127,7 @@ fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(),
             while current_value < limit {
                 // TODO: More efficiently.
                 let s = Scope::inherit(&s);
-                assign(&s, &[lua_syntax::Var::Name(var)], vec![Value::Number(current_value)])?;
+                assign(&s, &[lua_syntax::Var::Name(var)], vec![Value::from_number(current_value)])?;
                 interpret_block(&s, block)?;
                 current_value += step;
             }
@@ -333,7 +204,7 @@ fn interpret_statement<'a>(s: &Scope, stmt: &lua_syntax::Stat<'a>) -> Result<(),
     }
 }
 
-fn interpret_last_statement<'a>(s: &Scope, stmt: &lua_syntax::LastStat<'a>) -> Result<(), StatementError<'a>> {
+fn interpret_last_statement<'s, 'g>(s: &Scope, stmt: &lua_syntax::LastStat<'s>) -> Result<(), StatementError<'s, 'g>> {
     match stmt {
         lua_syntax::LastStat::Return(Some(exps)) => {
             Err(StatementError::Return(interpret_exps(s, exps)?))
@@ -343,25 +214,25 @@ fn interpret_last_statement<'a>(s: &Scope, stmt: &lua_syntax::LastStat<'a>) -> R
     }
 }
 
-fn define_function<'a>(s: &Scope, pars: &Option<lua_syntax::ParList<'a>>, body: &lua_syntax::Block<'a>) -> Result<Value<'a>, StatementError<'a>> {
+fn define_function<'s, 'g>(s: &Scope, pars: &Option<lua_syntax::ParList<'s>>, body: &lua_syntax::Block<'s>) -> Result<Value<'s, 'g>, StatementError<'s, 'g>> {
     eval_function(s, pars, body).map_err(StatementError::Error)
 }
 
-fn interpret_exps<'a>(s: &Scope, exps: &[lua_syntax::Exp<'a>]) -> Result<Vec<Value<'a>>, StatementError<'a>> {
+fn interpret_exps<'s, 'g>(s: &Scope, exps: &[lua_syntax::Exp<'s>]) -> Result<Vec<Value<'s, 'g>>, StatementError<'s, 'g>> {
     eval_exps(s, exps).map_err(StatementError::Error)
 }
 
-fn interpret_exp<'a>(s: &Scope, exp: &lua_syntax::Exp<'a>) -> Result<Vec<Value<'a>>, StatementError<'a>> {
+fn interpret_exp<'s, 'g>(s: &Scope, exp: &lua_syntax::Exp<'s>) -> Result<Vec<Value<'s, 'g>>, StatementError<'s, 'g>> {
     eval_exp(s, exp).map_err(StatementError::Error)
 }
 
-fn value_call<'a>(s: &Scope, f: &Value<'a>, args: &[Value<'a>]) -> Result<Vec<Value<'a>>, String> {
+fn value_call<'s, 'g>(s: &Scope, f: &Value<'s, 'g>, args: &[Value<'s, 'g>]) -> Result<Vec<Value<'s, 'g>>, String> {
     // TODO: Implement.
     eprintln!("value_call {:?} {:?}", f, args);
     Ok(vec![])
 }
 
-fn function_call<'a>(s: &Scope, fcall: &lua_syntax::FunctionCall<'a>) -> Result<Vec<Value<'a>>, String> {
+fn function_call<'s, 'g>(s: &Scope, fcall: &lua_syntax::FunctionCall<'s>) -> Result<Vec<Value<'s, 'g>>, String> {
     /*
     let prepare_args = |args: &lua_syntax::Args<'a>| -> Result<Vec<Value<'a>>, String> {
         match args {
@@ -376,12 +247,12 @@ fn function_call<'a>(s: &Scope, fcall: &lua_syntax::FunctionCall<'a>) -> Result<
     };
     match fcall {
         lua_syntax::FunctionCall::FunctionCall(f, args) => {
-            let f = adjust_single_value(eval_prefixexp(s, f)?);
+            let f = adjust_values_1(eval_prefixexp(s, f)?);
             let args = prepare_args(args)?;
             value_call(s, &f, &args)
         }
         lua_syntax::FunctionCall::MethodCall(f, method, args) => {
-            let f_self = adjust_single_value(eval_prefixexp(s, f)?);
+            let f_self = adjust_values_1(eval_prefixexp(s, f)?);
             let f = f_self.get_field(method);
             let mut args = prepare_args(args)?;
             args.insert(0, f_self);
@@ -394,13 +265,13 @@ fn function_call<'a>(s: &Scope, fcall: &lua_syntax::FunctionCall<'a>) -> Result<
     Ok(vec![])
 }
 
-fn eval_var<'a>(s: &Scope, var: &lua_syntax::Var<'a>) -> Result<Value<'a>, String> {
+fn eval_var<'s, 'g>(s: &Scope, var: &lua_syntax::Var<'s>) -> Result<Value<'s, 'g>, String> {
     // TODO: Implement.
     eprintln!("var {:?}", var);
-    Ok(Value::Nil)
+    Ok(Value::from_nil())
 }
 
-fn eval_prefixexp<'a>(s: &Scope, exp: &lua_syntax::PrefixExp<'a>) -> Result<Vec<Value<'a>>, String> {
+fn eval_prefixexp<'s, 'g>(s: &Scope, exp: &lua_syntax::PrefixExp<'s>) -> Result<Vec<Value<'s, 'g>>, String> {
     match exp {
         lua_syntax::PrefixExp::Var(var) => {
             let result = eval_var(s, var)?;
@@ -414,21 +285,21 @@ fn eval_prefixexp<'a>(s: &Scope, exp: &lua_syntax::PrefixExp<'a>) -> Result<Vec<
     }
 }
 
-fn eval_table_constructor<'a>(s: &Scope, table_ctor: &lua_syntax::TableCtor<'a>) -> Result<Value<'a>, String> {
+fn eval_table_constructor<'s, 'g>(s: &Scope, table_ctor: &lua_syntax::TableCtor<'s>) -> Result<Value<'s, 'g>, String> {
     // TODO: Implement.
     eprintln!("table_ctor {:?}", table_ctor);
-    Ok(Value::Nil)
+    Ok(Value::from_nil())
 }
 
-fn eval_binop<'a>(s: &Scope, op: lua_syntax::BinOp, lhs: &lua_syntax::Exp<'a>, rhs: &lua_syntax::Exp<'a>) -> Result<Vec<Value<'a>>, String> {
+fn eval_binop<'s, 'g>(s: &Scope, op: lua_syntax::BinOp, lhs: &lua_syntax::Exp<'s>, rhs: &lua_syntax::Exp<'s>) -> Result<Vec<Value<'s, 'g>>, String> {
     // TODO: Implement.
     eprintln!("eval_binop {:?} {:?} {:?}", op, lhs, rhs);
     Ok(vec![])
 }
 
-fn eval_unop<'a>(s: &Scope, op: lua_syntax::UnOp, exp: &lua_syntax::Exp<'a>) -> Result<Vec<Value<'a>>, String> {
+fn eval_unop<'s, 'g>(s: &Scope, op: lua_syntax::UnOp, exp: &lua_syntax::Exp<'s>) -> Result<Vec<Value<'s, 'g>>, String> {
     /*
-    let val = adjust_single_value(eval_exp(s, exp)?);
+    let val = adjust_values_1(eval_exp(s, exp)?);
     match op {
         lua_syntax::UnOp::Unm => {
             if let Some(num) = val.to_number() {
@@ -456,25 +327,25 @@ fn eval_unop<'a>(s: &Scope, op: lua_syntax::UnOp, exp: &lua_syntax::Exp<'a>) -> 
     Ok(vec![])
 }
 
-fn eval_varargs<'a>(s: &Scope) -> Result<Vec<Value<'a>>, String> {
+fn eval_varargs<'s, 'g>(s: &Scope) -> Result<Vec<Value<'s, 'g>>, String> {
     // TODO: Implement.
     eprintln!("eval_varargs");
     Ok(vec![])
 }
 
-fn eval_function<'a>(s: &Scope, pars: &Option<lua_syntax::ParList<'a>>, body: &lua_syntax::Block<'a>) -> Result<Value<'a>, String> {
+fn eval_function<'s, 'g>(s: &Scope, pars: &Option<lua_syntax::ParList<'s>>, body: &lua_syntax::Block<'s>) -> Result<Value<'s, 'g>, String> {
     // TODO: Implement.
     eprintln!("define_function {:?} {:?}", pars, body);
-    Ok(Value::Nil)
+    Ok(Value::from_nil())
 }
 
-fn eval_exp<'a>(s: &Scope, exp: &lua_syntax::Exp<'a>) -> Result<Vec<Value<'a>>, String> {
+fn eval_exp<'s, 'g>(s: &Scope, exp: &lua_syntax::Exp<'s>) -> Result<Vec<Value<'s, 'g>>, String> {
     match exp {
-        lua_syntax::Exp::Nil => Ok(vec![Value::Nil]),
-        lua_syntax::Exp::False => Ok(vec![Value::Bool(false)]),
-        lua_syntax::Exp::True => Ok(vec![Value::Bool(true)]),
-        lua_syntax::Exp::Number(n) => Ok(vec![Value::Number(*n)]),
-        lua_syntax::Exp::String(s) => Ok(vec![Value::StaticString(s.clone())]),
+        lua_syntax::Exp::Nil => Ok(vec![Value::from_nil()]),
+        lua_syntax::Exp::False => Ok(vec![Value::from_bool(false)]),
+        lua_syntax::Exp::True => Ok(vec![Value::from_bool(true)]),
+        lua_syntax::Exp::Number(n) => Ok(vec![Value::from_number(*n)]),
+        lua_syntax::Exp::String(s) => Ok(vec![Value::from_string(value::String::from_string_literal(s.clone()))]),
         lua_syntax::Exp::VarArgs => eval_varargs(s),
         lua_syntax::Exp::Function(body) => {
             let res = eval_function(s, &body.0, &body.1)?;
@@ -490,7 +361,7 @@ fn eval_exp<'a>(s: &Scope, exp: &lua_syntax::Exp<'a>) -> Result<Vec<Value<'a>>, 
     }
 }
 
-fn eval_exps<'a>(s: &Scope, exps: &[lua_syntax::Exp<'a>]) -> Result<Vec<Value<'a>>, String> {
+fn eval_exps<'s, 'g>(s: &Scope, exps: &[lua_syntax::Exp<'s>]) -> Result<Vec<Value<'s, 'g>>, String> {
     let mut result = Vec::with_capacity(exps.len());
     for e in exps {
         result.extend(eval_exp(s, e)?);
--- src/lua_interpreter/value.rs
+++ src/lua_interpreter/value.rs
@@ -1,11 +1,19 @@
+use crate::lua_lexemes::StringLiteral;
+
 #[derive(Debug, Clone)]
 pub struct TableRef<'s, 'g>(std::marker::PhantomData<&'s ()>, std::marker::PhantomData<&'g ()>);
 
 impl<'s, 'g> TableRef<'s, 'g> {
+    pub fn make(rows: &[Value<'s, 'g>]) -> TableRef<'s, 'g> {
+        // TODO: Implement.
+        eprintln!("TableRef::make {:?}", rows);
+        TableRef(std::marker::PhantomData, std::marker::PhantomData)
+    }
+
     pub fn get(&self, key: Value<'s, 'g>) -> Value<'s, 'g> {
         // TODO: Implement.
         eprintln!("TableRef::get {:?} {:?}", self, key);
-        Value::Nil
+        Value::from_nil()
     }
 
     pub fn set(&mut self, key: Value<'s, 'g>, value: Value<'s, 'g>) {
@@ -47,59 +55,134 @@ impl<'g> StringRef<'g> {
         StringRef(std::marker::PhantomData)
     }
 
-    pub fn len(&self) -> Number {
+    fn len(&self) -> Number {
         // TODO: Implement
         eprintln!("StringRef::len {:?}", self);
         0.0
     }
+
+    fn concat_ss<'s1, 's2>(s1: &'s1 str, s2: &'s2 str) -> StringRef<'g> {
+        // TODO: Implement
+        eprintln!("StringRef::concat_ss {:?} {:?}", s1, s2);
+        StringRef(std::marker::PhantomData)
+    }
+
+    fn concat_sd<'s>(s1: &'s str, s2: StringRef<'g>) -> StringRef<'g> {
+        // TODO: Implement
+        eprintln!("StringRef::concat_sd {:?} {:?}", s1, s2);
+        StringRef(std::marker::PhantomData)
+    }
+
+    fn concat_ds<'s>(s1: StringRef<'g>, s2: &'s str) -> StringRef<'g> {
+        // TODO: Implement
+        eprintln!("StringRef::concat_ds {:?} {:?}", s1, s2);
+        StringRef(std::marker::PhantomData)
+    }
+
+    fn concat_dd(s1: StringRef<'g>, s2: StringRef<'g>) -> StringRef<'g> {
+        // TODO: Implement
+        eprintln!("StringRef::concat_dd {:?} {:?}", s1, s2);
+        StringRef(std::marker::PhantomData)
+    }
 }
 
 #[derive(Debug, Clone)]
-pub struct FunctionRef<'g>(std::marker::PhantomData<&'g ()>);
+pub struct Varargs<'s, 'g>(std::marker::PhantomData<&'s ()>, std::marker::PhantomData<&'g ()>);
+
+impl<'s, 'g> Varargs<'s, 'g> {
+    fn make(vals: &[Value<'s, 'g>]) -> Varargs<'s, 'g> {
+        // TODO: Implement
+        eprintln!("Varargs::make {:?}", vals);
+        Varargs(std::marker::PhantomData, std::marker::PhantomData)
+    }
+
+    fn get(&self) -> Vec<Value<'s, 'g>> {
+        // TODO: Implement
+        eprintln!("Varargs::get {:?}", self);
+        vec![]
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct FunctionRef<'s, 'g>(std::marker::PhantomData<&'s ()>, std::marker::PhantomData<&'g ()>);
+
+#[derive(Debug, Clone)]
+pub struct FunctionInput<'s, 'g> {
+    pub global_env: TableRef<'s, 'g>,
+    pub captured_env: TableRef<'s, 'g>,
+    // Starts with function parameters.
+    pub local_env: TableRef<'s, 'g>,
+    pub varargs: Varargs<'s, 'g>,
+}
+
+impl<'s, 'g> FunctionRef<'s, 'g> {
+    fn make(f: impl Fn(FunctionInput<'s, 'g>) -> Vec<Value<'s, 'g>> + 'g, input: FunctionInput<'s, 'g>) -> FunctionRef<'s, 'g> {
+        // TODO: Implement
+        eprintln!("FunctionRef::make {:?}", input);
+        FunctionRef(std::marker::PhantomData, std::marker::PhantomData)
+    }
+
+    fn get_global_env(&self) -> TableRef<'s, 'g> {
+        // TODO: Implement
+        eprintln!("FunctionRef::get_env {:?}", self);
+        TableRef::make(&[])
+    }
+
+    fn set_global_env(&mut self, env: TableRef<'s, 'g>) {
+        // TODO: Implement
+        eprintln!("FunctionRef::set_env {:?} {:?}", self, env);
+    }
+}
 
 pub type Number = f64;
 
 #[derive(Debug, Clone)]
-pub enum String<'s, 'g> {
-    StaticString(&'s str),
+enum StringEnum<'s, 'g> {
+    StaticString(StringLiteral<'s>),
     DynamicString(StringRef<'g>),
 }
 
+#[derive(Debug, Clone)]
+pub struct String<'s, 'g>(StringEnum<'s, 'g>);
+
 impl<'s, 'g> String<'s, 'g> {
-    pub fn from_str(s: &'s str) -> String<'s, 'g> {
-        String::StaticString(s)
+    pub fn from_string_literal(s: StringLiteral<'s>) -> String<'s, 'g> {
+        String(StringEnum::StaticString(s))
     }
 
     pub fn to_number(&self) -> Option<Number> {
-        match self {
-            String::StaticString(s) => {
-                match s.parse::<Number>() {
+        match &self.0 {
+            StringEnum::StaticString(s) => {
+                match s.get_str().parse::<Number>() {
                     Ok(n) => Some(n),
                     Err(_) => None,
                 }
             }
-            String::DynamicString(s) => {
+            StringEnum::DynamicString(s) => {
                 s.to_number()
             }
         }
     }
 
     pub fn from_number(n: Number) -> String<'s, 'g> {
-        String::DynamicString(StringRef::from_number(n))
+        String(StringEnum::DynamicString(StringRef::from_number(n)))
     }
 
     pub fn concat(l: String<'s, 'g>, r: String<'s, 'g>) -> String<'s, 'g> {
-        // TODO: Implement
-        eprintln!("String::concat {:?} {:?}", l, r);
-        String::StaticString("")
+        match (l.0, r.0) {
+            (StringEnum::StaticString(s1), StringEnum::StaticString(s2)) => String(StringEnum::DynamicString(StringRef::concat_ss(s1.get_str(), s2.get_str()))),
+            (StringEnum::StaticString(s1), StringEnum::DynamicString(s2)) => String(StringEnum::DynamicString(StringRef::concat_sd(s1.get_str(), s2))),
+            (StringEnum::DynamicString(s1), StringEnum::StaticString(s2)) => String(StringEnum::DynamicString(StringRef::concat_ds(s1, s2.get_str()))),
+            (StringEnum::DynamicString(s1), StringEnum::DynamicString(s2)) => String(StringEnum::DynamicString(StringRef::concat_dd(s1, s2))),
+        }
     }
 
     pub fn len(&self) -> Number {
-        match self {
-            String::StaticString(s) => {
-                s.len() as Number
+        match &self.0 {
+            StringEnum::StaticString(s) => {
+                s.get_str().len() as Number
             }
-            String::DynamicString(s) => {
+            StringEnum::DynamicString(s) => {
                 s.len()
             }
         }
@@ -107,12 +190,12 @@ impl<'s, 'g> String<'s, 'g> {
 }
 
 #[derive(Debug, Clone)]
-pub enum Value<'s, 'g> {
+pub enum ValueEnum<'s, 'g> {
     Nil,
     Bool(bool),
     Number(Number),
     String(String<'s, 'g>),
-    Function(FunctionRef<'g>),
+    Function(FunctionRef<'s, 'g>),
     Table(TableRef<'s, 'g>),
     // TODO: userdata, thread.
 }
@@ -127,54 +210,64 @@ pub enum Type {
     Table,
 }
 
+#[derive(Debug, Clone)]
+pub struct Value<'s, 'g>(ValueEnum<'s, 'g>);
+
 impl<'s, 'g> Value<'s, 'g> {
     pub fn from_nil() -> Value<'s, 'g> {
-        Value::Nil
+        Value(ValueEnum::Nil)
     }
 
     pub fn from_bool(b: bool) -> Value<'s, 'g> {
-        Value::Bool(b)
+        Value(ValueEnum::Bool(b))
     }
 
     pub fn from_number(n: Number) -> Value<'s, 'g> {
-        Value::Number(n)
+        Value(ValueEnum::Number(n))
     }
 
     pub fn from_string(s: String<'s, 'g>) -> Value<'s, 'g> {
-        Value::String(s)
+        Value(ValueEnum::String(s))
     }
 
-    pub fn from_function(f: FunctionRef<'g>) -> Value<'s, 'g> {
-        Value::Function(f)
+    pub fn from_function(f: FunctionRef<'s, 'g>) -> Value<'s, 'g> {
+        Value(ValueEnum::Function(f))
     }
 
     pub fn from_table(t: TableRef<'s, 'g>) -> Value<'s, 'g> {
-        Value::Table(t)
+        Value(ValueEnum::Table(t))
     }
 
     pub fn get_type(&self) -> Type {
-        match self {
-            Value::Nil => Type::Nil,
-            Value::Bool(_) => Type::Bool,
-            Value::Number(_) => Type::Number,
-            Value::String(_) => Type::String,
-            Value::Function(_) => Type::Function,
-            Value::Table(_) => Type::Table,
+        match self.0 {
+            ValueEnum::Nil => Type::Nil,
+            ValueEnum::Bool(_) => Type::Bool,
+            ValueEnum::Number(_) => Type::Number,
+            ValueEnum::String(_) => Type::String,
+            ValueEnum::Function(_) => Type::Function,
+            ValueEnum::Table(_) => Type::Table,
+        }
+    }
+
+    pub fn is_nil(&self) -> bool {
+        match self.0 {
+            ValueEnum::Nil => true,
+            _ => false
         }
     }
 
     pub fn to_bool(&self) -> bool {
-        match self {
-            Value::Nil => false,
-            Value::Bool(b) => *b,
+        match self.0 {
+            ValueEnum::Nil => false,
+            ValueEnum::Bool(b) => b,
             _ => true,
         }
     }
 
     pub fn to_number(&self) -> Option<Number> {
-        match self {
-            Value::Number(n) => Some(*n),
-            Value::String(s) => {
+        match &self.0 {
+            ValueEnum::Number(n) => Some(*n),
+            ValueEnum::String(s) => {
                 s.to_number()
             }
             _ => None,
@@ -182,24 +275,36 @@ impl<'s, 'g> Value<'s, 'g> {
     }
 
     pub fn to_string(&self) -> Option<String<'s, 'g>> {
-        match self {
-            Value::Number(n) => Some(String::from_number(*n)),
-            Value::String(s) => Some(s.clone()),
+        match &self.0 {
+            ValueEnum::Number(n) => Some(String::from_number(*n)),
+            ValueEnum::String(s) => Some(s.clone()),
             _ => None,
         }
     }
 
     pub fn as_table(&self) -> Option<&TableRef<'s, 'g>> {
-        match self {
-            Value::Table(t) => Some(t),
+        match &self.0 {
+            ValueEnum::Table(t) => Some(t),
             _ => None,
         }
     }
 
-    pub fn as_function(&self) -> Option<&FunctionRef<'g>> {
-        match self {
-            Value::Function(f) => Some(f),
+    pub fn as_function(&self) -> Option<&FunctionRef<'s, 'g>> {
+        match &self.0 {
+            ValueEnum::Function(f) => Some(f),
             _ => None,
         }
+    }
+}
+
+pub fn adjust_values<'s, 'g>(mut vals: Vec<Value<'s, 'g>>, count: usize) -> Vec<Value<'s, 'g>> {
+    vals.resize_with(count, || Value::from_nil());
+    vals
+}
+
+pub fn adjust_values_1<'s, 'g>(vals: Vec<Value<'s, 'g>>) -> Value<'s, 'g> {
+    match vals.into_iter().next() {
+        Some(val) => val,
+        None => Value::from_nil(),
     }
 }
